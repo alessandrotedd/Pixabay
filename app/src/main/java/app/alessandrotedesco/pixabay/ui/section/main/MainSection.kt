f5e2b599a7111d7b4313bf352801ca248225eac6
Alessandro Tedesco <spam.alessandrotedd@gmail.com>
2023-06-11 22:48:48 +0200

    Add search bar icon & placeholder

--- app/src/main/java/app/alessandrotedesco/pixabay/ui/section/main/MainSection.kt
+++ app/src/main/java/app/alessandrotedesco/pixabay/ui/section/main/MainSection.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -23,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -87,7 +89,17 @@ fun MainSectionUI(
             query = query.value,
             onQueryChange = { query.value = it },
             active = true,
-            onActiveChange = { }
+            onActiveChange = { },
+            leadingIcon = {
+                androidx.compose.foundation.Image(
+                    painter = painterResource(id = R.drawable.search),
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }, placeholder = {
+                Text("Search images")
+            }
         ) {
             images?.let {
                 if (images.isEmpty()) {
